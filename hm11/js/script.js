52840d118ebd7a0c63afe94e8078661d619a3f5b
Gherman Marina <gherman.marina@icloud.com>
2020-03-20 15:49:54 +0100

    server

--- hm11/js/script.js
+++ hm11/js/script.js
@@ -115,5 +115,53 @@ setClock('timer', deadline);
         document.body.style.overflow = '';
     });
 
+    //form
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'что-то пошло не так..'
+    };
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+    
+        statusMessage.classList.add('status');
+        form.addEventListener('submit', function(event) {
+                event.preventDefault();
+                form.appendChild(statusMessage);
+
+                let request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+                request.setRequestHeader ('Content-type', 'applcation/x-www-form-urlencoded'); // for JSON 'application/json; charset=utf-8'
+
+                let formData = new FormData(form);
+
+
+                /*для предбразования formData in JSON
+
+                    let obj ={};
+                    formData.forEach(function(value, key) {
+                        obj[key] = value;
+                    });
+                    let json  = JSON.stringify(obj);
+                     request.send(json);
+                     */
+                
+                
+                request.send(formData);
+                request.addEventListener('readystatechange', function() {
+                    if (request.readyState < 4) {
+                        statusMessage.innerHTML = message.loading;
+                    } else if (request.readyState === 4 && request.status == 200) {
+                        statusMessage.innerHTML = message.success;
+                    } else {
+                        statusMessage.innerHTML = message.failure;
+                    }
+                })
+                for ( let i =0; i < input.length; i++) {
+                    input[i].value = '';
+                }
+        });
+
 
 });
