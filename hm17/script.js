a0dcde2c214dd6e814d962b1c3d00e729c3136cc
Gherman Marina <gherman.marina@icloud.com>
2020-03-31 16:16:30 +0200

    jquery

---
+++ hm17/script.js
@@ -0,0 +1,36 @@
+
+$('.main_btna').on('click',function() {
+    $('.overlay').show('slow');
+    $('.modal').show('slow');
+
+});
+
+$('.close').on('click',function() {
+    $('.modal').hide('slow');
+    $('.overlay').hide('slow');
+
+});
+
+$('.main_btn').on('click',function() {
+    $('.overlay').show('slow');
+    $('.modal').show('slow');
+
+});
+
+$('.close').on('click',function() {
+    $('.modal').hide('slow');
+    $('.overlay').hide('slow');
+
+});
+
+$('a[href="#sheldure"]').on('click',function() {
+    $('.overlay').show('slow');
+    $('.modal').show('slow');
+
+});
+
+$('.close').on('click',function() {
+    $('.modal').hide('slow');
+    $('.overlay').hide('slow');
+
+});
